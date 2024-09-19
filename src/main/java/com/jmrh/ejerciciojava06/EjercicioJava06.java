7874e216d6a40e65b846f3d72e5d21e55a1e4938
Andressalasf <humkyjimp@gmail.com>
2024-09-19 11:26:10 -0500

    Añadiendo la solucion de complejidad de la clase principal y actualizando la clase tablero

--- src/main/java/com/jmrh/ejerciciojava06/EjercicioJava06.java
+++ src/main/java/com/jmrh/ejerciciojava06/EjercicioJava06.java
@@ -3,20 +3,23 @@ package com.jmrh.ejerciciojava06;
 
 import java.util.Scanner;
 
-public class EjercicioJava06 {
+// Andres Felipe Salas Niño -192164
+// Fabian Acosta - 192168
 
-    private static Tablero tablero; //1
+public class EjercicioJava06 { 
+
+    private static Tablero tablero; //1 + (3n**2+25n+35) = 3n**2+25n+36
     private static Scanner scanner; //1 
     private static boolean partidaEnJuego; //1
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //1 --> (114n**4 + 156n**3 + 585**2 + 425n +149) ; o(n**4)
 
         scanner = new Scanner(System.in); //1
-        menuPrincipal(); // 26n+2
+        menuPrincipal(); // 1 + (114n**4 + 156n**3 + 585**2 + 425n +145) = 114n**4 + 156n**3 + 585**2 + 425n +146
 
-    }
+    } // 1
 
-    public static void menuPrincipal() {
+    public static void menuPrincipal() { // 1 --> (114n**4 + 156n**3 + 585**2 + 425n +145) ; o(n**4)
 
         int opcion;//1
 
@@ -31,15 +34,15 @@ public class EjercicioJava06 {
                 opcion = Integer.parseInt(scanner.nextLine()); //n
             } catch (Exception e) { //n
                 opcion = 0; //n
-            }
+            } // n
 
             switch (opcion) {//n
                 case 1: //n
-                    iniciarPartida(); //()
+                    iniciarPartida(); // 1 + (57n**4+78n**3+294n**2+212n+88) = 57n**4+78n**3+294n**2+212n+89
                     break; //n
                 case 2: //n
                     if(partidaEnJuego)// n
-                        continuarPartida();// ()
+                        continuarPartida();// 1 + (57n**4+78n**3+291n**2+187n+51) = 57n**4+78n**3+291n**2+187n+52
                     else //n
                         System.out.println("No hay partida en juego"); //n
                     break;//n
@@ -48,58 +51,58 @@ public class EjercicioJava06 {
                     break; //n
                 default: //n
                     System.out.println("Opción desconocida"); //n
-            }
+            } // n
 
         } while (opcion != 3); //n+1
 
-    }
+    } // 1
 
-    public static void iniciarPartida() {
-        tablero = new Tablero(); //1
+    public static void iniciarPartida() { //1 --> (57n**4+78n**3+294n**2+212n+88) ; o(n**4)
+        tablero = new Tablero(); // 1 + (3n**2+25n+35) = 3n**2+25n+36
         partidaEnJuego = true; //1
-        juego();
-    }
+        juego(); // 1 + (57n**4+78n**3+291n**2+187n+48) = 57n**4+78n**3+291n**2+187n+49
+    } // 1
     
-    public static void continuarPartida() {
-        juego();
-    }
+    public static void continuarPartida() { // 1 --> 57n**4+78n**3+291n**2+187n+51 ; o(n**4)
+        juego(); // 1 + (57n**4+78n**3+291n**2+187n+48) = 57n**4+78n**3+291n**2+187n+49
+    } // 1
 
-    public static void juego() {
+    public static void juego() { // 1 --> (57n**4+78n**3+291n**2+187n+48) ; o(n**4)
         String direccion; //1
 
         do { //n
-        tablero.mostrar();
-        System.out.println("izq (a) / der (d) / arriba (w) / abajo (s) / menu (m): ");
-        direccion = scanner.nextLine();
+        tablero.mostrar(); // n * (9n**3+29n**2+24n+9) = 9n**4+29n**3+24n**2+9n 
+        System.out.println("izq (a) / der (d) / arriba (w) / abajo (s) / menu (m): "); // n
+        direccion = scanner.nextLine(); // n
 
-        switch (direccion) {
-            case "w":
-                tablero.moverArriba();
-                break;
-            case "s":
-                tablero.moverAbajo();
-                break;
-            case "a":
-                tablero.moverIzquierda();
-                break;
-            case "d":
-                tablero.moverDerecha();
-                break;
-            case "m":
-                System.out.println("Volviendo al menu...");
-                break;
-            default:
-                System.out.println("Opción desconocida");
-        }
+        switch (direccion) { // n
+            case "w": // n
+                tablero.moverArriba(); // n * (12n**3+10n**2+54n+30) = 12n**4+10n**3+54n**2+30n
+                break; // n
+            case "s": // n
+                tablero.moverAbajo(); // n * (12n**3+10n**2+54n+30)  = 12n**4+10n**3+54n**2+30n
+                break; // n
+            case "a": // n
+                tablero.moverIzquierda(); // n * (12n**3+10n**2+54n+30)  = 12n**4+10n**3+54n**2+30n
+                break; // n
+            case "d": // n
+                tablero.moverDerecha(); // n * (12n**3+10n**2+54n+30)  = 12n**4+10n**3+54n**2+30n
+                break; // n
+            case "m": // n
+                System.out.println("Volviendo al menu..."); // n
+                break; // n
+            default: // n
+                System.out.println("Opción desconocida"); // n
+        } // n
         
-        } while(!direccion.equals("m") && !tablero.finPartida());
+        } while(!direccion.equals("m") && !tablero.finPartida()); // n+1 + (9n**2+6n+12) = 9n**2+7n+13
 
-        if (tablero.finPartida()) {
-            tablero.mostrar();
-            String mensaje = tablero.ganador() ? "Has Ganado!!!" : "Has perdido";
-            System.out.println(mensaje);
-            partidaEnJuego = false;
-        }
+        if (tablero.finPartida()) { // 1 + (9n**2+6n+12) = 9n**2+6n+13
+            tablero.mostrar(); // 1 + (9n**3+29n**2+24n+9) = 9n**3+29n**2+24n+10
+            String mensaje = tablero.ganador() ? "Has Ganado!!!" : "Has perdido"; //  1 + (4n**2 +3n + 4) = 4n**2 +3n + 5
+            System.out.println(mensaje); // 1
+            partidaEnJuego = false; // 1
+        } // 1
 
-    }
-}
+    } // 1
+} // 1
--- src/main/java/com/jmrh/ejerciciojava06/Tablero.java
+++ src/main/java/com/jmrh/ejerciciojava06/Tablero.java
@@ -3,6 +3,8 @@ package com.jmrh.ejerciciojava06;
 
 import java.util.Random;
 
+// Andres Felipe Salas Niño -192164
+// Fabian Acosta - 192168
 
 public class Tablero { //1
     
@@ -10,7 +12,7 @@ public class Tablero { //1
     public static final int COL = 4; //1
     private final int[][] tablero; //1
     
-    public Tablero() { //1 --> (3n**2+25n+32)
+    public Tablero() { //1 --> (3n**2+25n+32) + 3 --> O(n**2)
         tablero = new int[FIL][COL]; //1
         for(int f=0;f<FIL;f++) { // n+1
             for(int c=0;c<COL;c++) { // (n+1)*n --> n**2+n
@@ -24,7 +26,7 @@ public class Tablero { //1
                
     }//1
     
-    private int vaciasEnFila(int f) { //1  -->  (5n+5) ; n=4 ; 25
+    private int vaciasEnFila(int f) { //1  -->  (5n+5) ; O(n)
         int n=0; //1
         for(int c=0;c<COL;c++) { // n+1
             if(tablero[f][c]==0) { // n
@@ -34,7 +36,7 @@ public class Tablero { //1
         return n; //1
     } //1
     
-    private int vaciasEnColumna(int c) { //1  -->  (5n+5) ; n=4 ; 25
+    private int vaciasEnColumna(int c) { //1  -->  (5n+5) ; O(n)
         int n=0; //1
         for(int f=0;f<FIL;f++) { // n+1
             if(tablero[f][c]==0) { //n
@@ -44,7 +46,7 @@ public class Tablero { //1
         return n;//1
     } // 1
     
-    private int vacias() { //1  --> (5n**2+3n+5) ; n=4 ; 97
+    private int vacias() { //1  --> (5n**2+3n+5) ; o(n**2)
         int n=0; //1
         for(int f=0;f<FIL;f++) { //n+1
             for(int c=0;c<COL;c++) { // (n+1)*n --> n**2+n
@@ -57,7 +59,7 @@ public class Tablero { //1
     } //1
     
     
-    private void ponerDos() { //1  --> (11n+13) ; n=4 ; 57
+    private void ponerDos() { //1  --> (11n+13) ; o(n)
         int f; //1
         int c; //1
         
@@ -76,7 +78,7 @@ public class Tablero { //1
     }//1
     
     
-    public void mostrar() { // 1   -->   (9n**3+29n**2+24n+8); n=4 ; 1144
+    public void mostrar() { // 1   -->   (9n**3+29n**2+24n+9); o(n**3)
         for(int f=0;f<FIL;f++) { // n+1
             dibujaLineaHorizontal(); // (3n**2+4n+5)*n = 3n**3+ 4n**2+5n
             dibujaEspacioHorizontal(); // (3n**2+4n+5)*n = 3n**3+ 4n**2+5n
@@ -98,10 +100,10 @@ public class Tablero { //1
             System.out.print("\n"); //n
             dibujaEspacioHorizontal(); // (3n**2+4n+5)*n = 3n**3+ 4n**2+5n
         } //n
-        dibujaLineaHorizontal(); //(3n**2+4n+5)
+        dibujaLineaHorizontal(); // 1 + (3n**2+4n+5) = 3n**2+4n+6
     } // 1 
     
-    private void dibujaLineaHorizontal() { //1   -->  (3n**2+4n+5) ; n=4 ; 69
+    private void dibujaLineaHorizontal() { //1   -->  (3n**2+4n+5) ; o(n**2)
         
         System.out.print("|"); // 1
         for(int c=0;c<COL;c++) { // n+1
@@ -114,7 +116,7 @@ public class Tablero { //1
         
     } //1
     
-    private void dibujaEspacioHorizontal() { //1  -->   ( 3n**2+4n+5) ; n=4 ; 69
+    private void dibujaEspacioHorizontal() { //1  -->   ( 3n**2+4n+5) ; o(n**2)
          System.out.print("|"); //1
         for(int c=0;c<COL;c++) { // n+1
             for(int i=0;i<12;i++) { // (n+1)*n --> n**2+n
@@ -125,7 +127,7 @@ public class Tablero { //1
         System.out.print("\n"); //1
     }//1
     
-    public boolean ganador() { //1 --> (4n**2 +3n + 4)
+    public boolean ganador() { //1 --> (4n**2 +3n + 4) ; o(n**2)
         for(int f=0;f<FIL;f++) {    //n+1      
             for(int c=0;c<COL;c++) {  // (n+1) * n --> n**2+n
                 if(tablero[f][c]==2048) return true; // n**2 + n**2
@@ -134,11 +136,11 @@ public class Tablero { //1
         return false; //1
     } //1
     
-    public boolean finPartida() { //1 --> (9n**2+6n+12)
+    public boolean finPartida() { //1 --> (9n**2+6n+12) ; o(n**2)
         return (ganador() || vacias()==0); // 1 + (4n**2 +3n + 4) + (5n**2+3n+5) --> 9n**2+6n+10
     } // 1
     
-    public void moverArriba() { // 1 --> (12n**3+10n**2+54n+30)
+    public void moverArriba() { // 1 --> (12n**3+10n**2+54n+30) ; o(n**3)
         for(int c=0; c<COL; c++) { //n+1
             moverArribaColumna(c); // n * (12n**2+1n+35) = 12n**3+1n**2+35n
         } // n
@@ -146,13 +148,13 @@ public class Tablero { //1
                 ponerDos(); // 1 + (11n+13) = 11n+14
     } // 1
     
-    private void moverArribaColumna(int c) { // 1 --> (12n**2+1n+35)
+    private void moverArribaColumna(int c) { // 1 --> (12n**2+1n+35) ; o(n**2)
         colocarArriba(c); // 1+ (6n**2-5n+14) = 6n**2-5n+15
         sumarArriba(c); // 1+ (11n+4) = 11n+5
         colocarArriba(c); //  1+ (6n**2-5n+14) = 6n**2-5n+15
     }
     
-    private void colocarArriba(int c) { // 1  --> (6n**2-5n+14)
+    private void colocarArriba(int c) { // 1  --> (6n**2-5n+14) ; o (n**2)
         
         if(vaciasEnColumna(c) < COL) { // (5n+5) + 1+ 1 = 5n+7
             for(int veces = 0;veces < COL-1;veces++) { // n-1
@@ -168,7 +170,7 @@ public class Tablero { //1
     } // 1
     
     
-    private void sumarArriba(int c) { // 1 (11n+4)
+    private void sumarArriba(int c) { // 1 (11n+4) ; o(n)
         if(vaciasEnColumna(c)<FIL-1) { // (5n+5) + 1 +1 = 5n+7
             for(int f=0;f<FIL-1;f++) { // n-1
                 if(tablero[f][c] == tablero[f+1][c]) { //n-1
@@ -183,7 +185,7 @@ public class Tablero { //1
     
     
     
-    public void moverAbajo() { // 1 --> (12n**3+10n**2+54n+30)
+    public void moverAbajo() { // 1 --> (12n**3+10n**2+54n+30) ; o(n**3)
         for(int c=0; c<COL; c++) { // n+1
             moverAbajoColumna(c); // n * (12n**2+1n+35) = 12n**3+1n**2+35n
         } //n
@@ -191,13 +193,13 @@ public class Tablero { //1
                 ponerDos(); // 1 + (11n+13) = 11n+14
     } // 1
     
-    private void moverAbajoColumna(int c) { // 1 --> (12n**2+1n+35)
+    private void moverAbajoColumna(int c) { // 1 --> (12n**2+1n+35) ; o(n**2)
         colocarAbajo(c); // 1 + (6n**2-5n+14) = 6n**2-5n+15
         sumarAbajo(c); // 1+ (11n+4) = 11n+5
         colocarAbajo(c); //  1 + (6n**2-5n+14) = 6n**2-5n+15
     }
     
-    private void colocarAbajo(int c) { // 1 --> (6n**2-5n+14)
+    private void colocarAbajo(int c) { // 1 --> (6n**2-5n+14) ; o(n**2)
         
         if(vaciasEnColumna(c) < COL) { // 1 + (5n+5) + 1 = 5n+7
             for(int veces = 0;veces < COL-1;veces++) { // n-1
@@ -213,7 +215,7 @@ public class Tablero { //1
     } //1
     
     
-    private void sumarAbajo(int c) { // 1 --> (11n+4)
+    private void sumarAbajo(int c) { // 1 --> (11n+4) ; o(n)
         if(vaciasEnColumna(c)<FIL-1) { // 1 + 1  + (5n+5) = 5n+7
             for(int f=FIL-1;f>0;f--) { // n-1
                 if(tablero[f][c] == tablero[f-1][c]) { // n-1
@@ -226,7 +228,7 @@ public class Tablero { //1
     
     
     
-    public void moverDerecha() { // 1 --> (12n**3+10n**2+54n+30)
+    public void moverDerecha() { // 1 --> (12n**3+10n**2+54n+30) ; o(n**3)
         for(int f=0; f<COL; f++) { // n+1
             moverDerechaFila(f); // n * (12n**2+1n+35) = 12n**3+1n**2+35n
         } //n
@@ -234,13 +236,13 @@ public class Tablero { //1
                 ponerDos();  // 1 + (11n+13) = 11n+14
     }
     
-    private void moverDerechaFila(int f) { // 1 --> (12n**2+1n+35)
+    private void moverDerechaFila(int f) { // 1 --> (12n**2+1n+35) ; o(n**2)
         colocarDerecha(f); // 1 + (6n**2-5n+14) = 6n**2-5n+15
         sumarDerecha(f); // 1 + (11n+4) = 11n+5
         colocarDerecha(f); // 1 + (6n**2-5n+14) = 6n**2-5n+15
     }
     
-    private void colocarDerecha(int f) { // 1 --> (6n**2-5n+14)
+    private void colocarDerecha(int f) { // 1 --> (6n**2-5n+14) ; o(n**2)
         
         if(vaciasEnFila(f) < FIL) { // 1 + 1 + (5n+5) = 5n+7
             for(int veces = 0;veces < COL-1;veces++) { // n-1
@@ -256,7 +258,7 @@ public class Tablero { //1
     } // 1
     
     
-    private void sumarDerecha(int f) { // 1 --> (11n+4)
+    private void sumarDerecha(int f) { // 1 --> (11n+4) ; o(n)
         if(vaciasEnFila(f)<FIL-1) { // 1 + 1 + (5n+5) = 5n+7
             for(int c=COL-1;c>0;c--) { // n-1
                 if(tablero[f][c] == tablero[f][c-1]) { // n-1
@@ -268,7 +270,7 @@ public class Tablero { //1
     } //1
     
     
-    public void moverIzquierda() { //1 --> (12n**3+10n**2+54n+30)
+    public void moverIzquierda() { //1 --> (12n**3+10n**2+54n+30) ; o (n**3)
         for(int f=0; f<FIL; f++) { // n+1
             moverIzquierdaFila(f); // n * (12n**2+1n+35) = 12n**3+1n**2+35n
         } //n
@@ -276,13 +278,13 @@ public class Tablero { //1
                 ponerDos(); // 1 + (11n+13) = 11n+14
     }//1
     
-    private void moverIzquierdaFila(int f) { // 1 --> (12n**2+1n+35)
+    private void moverIzquierdaFila(int f) { // 1 --> (12n**2+1n+35) ; o(n**2)
         colocarIzquierda(f); // 1 + (6n**2-5n+14) = 6n**2-5n+15
         sumarIzquierda(f); // 1 + (11n+4) = 11n+5
         colocarIzquierda(f); // 1 + (6n**2-5n+14) = 6n**2-5n+15
     }
     
-    private void colocarIzquierda(int f) { //1 (6n**2-5n+14)
+    private void colocarIzquierda(int f) { //1 (6n**2-5n+14) ; o(n**2)
         
         if(vaciasEnFila(f) < FIL) { // (comparacion = 1) + (5n+5) + (llamada al metodo = 1) -->5n+7
             for(int veces = 0;veces < COL-1;veces++) { // n-1
@@ -298,7 +300,7 @@ public class Tablero { //1
     } // 1
     
     
-    private void sumarIzquierda(int f) { //1   (11n+4)
+    private void sumarIzquierda(int f) { //1   (11n+4) ; o(n)
         if(vaciasEnFila(f)<FIL-1) { // (comparacion = 1) + (5n+5) + (llamada al metodo = 1)  -->5n+7
             for(int c=0;c<COL-1;c++) { // n-1
                 if(tablero[f][c] == tablero[f][c+1]) { // n -1
